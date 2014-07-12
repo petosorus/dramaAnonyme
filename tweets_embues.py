6f8a616bc77098f85214ee53a9ca884e1bfd7583
petosorus <petosorus@hotmail.fr>
2014-07-12 07:13:51 +0200

    Mise en forme + commentaire

--- tweets_embues.py
+++ tweets_embues.py
@@ -8,6 +8,7 @@ from random import randrange
 import re
 from time import localtime
 
+
 def file_to_dict(file_name):
 	"""opens a file and put tab separated values in a dict"""
 	f = open(file_name, 'r')
@@ -18,6 +19,7 @@ def file_to_dict(file_name):
 		d_keys[j[0]] = j[1]
 	return d_keys
 
+
 def twitter_authentification(keys_file):
 	"""return a tweepy.api object connected to your account"""
 	d_keys = file_to_dict(keys_file)
@@ -27,6 +29,7 @@ def twitter_authentification(keys_file):
 		d_keys["access_secret"])
 	api = tweepy.API(auth)
 	return api
+
 
 def daemonize():
     try:
@@ -52,13 +55,16 @@ def daemonize():
     print pid
     return(0)
 
+
 def is_direct_mention(text):
 	pattern_direct_mention = re.compile("^@.+")
 	return re.match(pattern_direct_mention, text)
+
 	
 def contains_link(text):
 	pattern_link = re.compile(".*?http.*?")
 	return re.match(pattern_link, text)
+
 
 def parsing_dm_list(dm_list):
 	dm_list_copy = []
@@ -66,30 +72,36 @@ def parsing_dm_list(dm_list):
 		if not is_direct_mention(dm.text) and not contains_link(dm.text):
 			dm_list_copy.append(dm)
 	return dm_list_copy
+
 	
 def display_time():
 	time = localtime()
 	return str(time[3]) + "h" + str(time[4]) + "min"
+
 	
 def logging_tweets(dm):
 	f = open("tweets.log", 'a')
 	f.write(display_time() + dm.sender.screen_name.encode('utf-8') + "\t" + 
 		dm.text.encode('utf-8') + "\t" + "\n")
 	f.close()
+
 	
 def erasing_last_dm_batch(last_dm_id):
 	old_dms = api.direct_messages(0, last_dm_id)
 	for dm in old_dms:
 		api.destroy_direct_message(dm.id)
+
+
+
 	
 if __name__ == "__main__":
-	#retCode = daemonize()
+#	retCode = daemonize()
 	cur_dir = os.path.dirname(os.path.realpath(__file__))
 	
 	api = twitter_authentification(cur_dir + "/keys.conf")
 	dms = api.direct_messages()
 	dms = parsing_dm_list(dms)
-	#first_dm_id = dms[0].id
+#	first_dm_id = dms[0].id
 	time_dilatation = { 0 : 1800,
 						1 : 900,
 						2 : 600,
@@ -106,28 +118,36 @@ if __name__ == "__main__":
 			nb_dms = len(dms)
 			print "Nb de DM : " + str(nb_dms)
 			
-			if nb_dms != 0 and flag:
+			if nb_dms != 0 and flag:	#Flag car bug au lancement, épuisement 
+										#de tous les dm sans passer par 
+										#les sleep. Incompréhensible
+										
 				flag = False
+				
+				#Choix d'un dm au hasard dans la liste
 				print "Picking dm"
 				picked_dm = dms[randrange(nb_dms)]
 				
+				#Envoi du tweet
 				print picked_dm.text
 				api.update_status(picked_dm.text)
 				
 				logging_tweets(picked_dm)	
-				dms.remove(picked_dm)
+				dms.remove(picked_dm)	#Redondance si jamais aucune liste 
+										#n'est récupérée en fin de cycle
 				api.destroy_direct_message(picked_dm.id)
 				
-				last_dm_id = dms[nb_dms - 1].id
-				first_dm_id = last_dm_id
+#				last_dm_id = dms[nb_dms - 1].id
+#				first_dm_id = last_dm_id
 
-
+			#Définition du temps d'attente et premier temps d'attente
 			if nb_dms >= 8:
 				nb_dms = 8
-			print display_time() + " - Time before following : " + time_dilatation[nb_dms]
+			print display_time() + " - Time before following : " + 
+				time_dilatation[nb_dms]
 			sleep(time_dilatation[nb_dms])
 		
-		
+			#Follow back des derniers followers
 			print "Follow back"
 			followers = api.followers()				
 			for follower in followers:
@@ -136,19 +156,29 @@ if __name__ == "__main__":
 						follower.follow()
 					except tweepy.error.TweepError:pass
 			
-			print display_time() + " - Time before tweeting : " + time_dilatation[nb_dms]
+			#Définition du temps d'attente et second temps d'attente
+			print display_time() + " - Time before tweeting : " + 
+				time_dilatation[nb_dms]
 			sleep(time_dilatation[nb_dms])
 			
 			try:
-				#dms = api.direct_messages(first_dm_id)
-				dms = api.direct_messages()				
-			#	try:
-			#		erasing_last_dm_batch(last_dm_id)
-			#	except NameError:
-			#		last_dm_id = first_dm_id
-				dms = parsing_dm_list(dms)
+#				dms = api.direct_messages(first_dm_id) #A utiliser pour purge 
+														#régulière de la liste.
+														#Si le compte grossit.
+														
+				dms = api.direct_messages()				#Récupération classique 
+														#de la list de dms
+				
+#				try:		#Probabilité de variable non-déclarée
+#							#N'arrive qu'avec une liste vide au 1er cycle
+#			
+#					erasing_last_dm_batch(last_dm_id)	
+#				except NameError:
+#					last_dm_id = first_dm_id
 
 			except tweepy.error.TweepError:pass
+			
+			dms = parsing_dm_list(dms)
 			flag = True
 		except IndexError:pass
 	
